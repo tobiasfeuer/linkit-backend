e989d2208600d3d845e09e9474cd3bb370c35a08
juanmeyer94 <juan_meyer@hotmail.com.ar>
2026-06-16 11:02:40 -0300

    contact routes ID

--- src/resources/infrastructure/routes/contactus.routes.ts
+++ src/resources/infrastructure/routes/contactus.routes.ts
@@ -22,6 +22,7 @@ const formLimiter = rateLimit({
 })
 
 const contactUsRoute = Router()
+const contactTableId = process.env.AIRTABLE_CONTACT_TABLE_ID ?? ''
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 contactUsRoute.post('/', formLimiter, async (req, res): Promise<any> => {
@@ -39,7 +40,7 @@ contactUsRoute.post('/', formLimiter, async (req, res): Promise<any> => {
       }
     }
 
-    await base('Contactos').create([record])
+    await base(contactTableId).create([record])
     const response: translatedResponse = { en: 'Your information has been sent successfully', es: 'Tu informacion ha sido enviada con exito' }
     res.status(200).send(response[(req as any).lang as keyof translatedResponse])
   } catch (error: any) {
@@ -65,7 +66,7 @@ contactUsRoute.post('/form', formLimiter, async (req, res): Promise<any> => {
       }
     }
 
-    await base('Contactos').create([record])
+    await base(contactTableId).create([record])
     const response: translatedResponse = { 
       en: 'Your information has been sent successfully', 
       es: 'Tu información ha sido enviada con éxito' 
